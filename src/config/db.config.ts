2bddb33f03fb6a5164021c3b6fa42e63210d1037
mrj4001 <mrj4001@med.cornell.edu>
2024-01-17 16:20:13 +0530

    made the changes to read the secrets from the AWS SecretManager.

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,23 +1,39 @@
-import { Sequelize } from "sequelize"
+import { Sequelize} from "sequelize";
+import  {getSecret} from '../utils/secretsManager';
+
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-    process.env.RECITER_DB_NAME || "",
-    process.env.RECITER_DB_USERNAME || "",
-    process.env.RECITER_DB_PASSWORD,
-    {
-        dialect: 'mysql',
-        dialectOptions: {
-            connectTimeout: 120000, // 60 seconds
-        },
-        host:  process.env.RECITER_DB_HOST  ,
-        port: 3306,
-        pool: {
-            max: 20,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-})
+   
+export async function initializeSequelize() {
 
-export default sequelize
+    try {
+        // Retrieve secrets from AWS Secrets Manager
+            const reciterPubSecretManager = process.env.RECITER_PUB_SECRET_MANAGER || '';
+            const dbSecrets = await getSecret( reciterPubSecretManager);
+            const { RECITER_DB_NAME,RECITER_DB_USERNAME,RECITER_DB_PASSWORD,RECITER_DB_HOST,RECITER_DB_REGION, RECITER_DB_PORT } = dbSecrets;
+            const sequelize = new Sequelize(
+                RECITER_DB_NAME || process.env.RECITER_DB_NAME || "",
+                RECITER_DB_USERNAME || process.env.RECITER_DB_USERNAME || "",
+                RECITER_DB_PASSWORD || process.env.RECITER_DB_PASSWORD || "",
+                {
+                    dialect: 'mysql',
+                    dialectOptions: {
+                        connectTimeout: 120000, // 60 seconds
+                    },
+                    host: RECITER_DB_HOST || process.env.RECITER_DB_HOST  ,
+                    port:  RECITER_DB_PORT || 3306,
+                    pool: {
+                        max: 20,
+                        min: 0,
+                        acquire: 30000,
+                        idle: 10000
+                    }
+                }
+            );
+            return sequelize; 
+        }
+        catch (error) {
+            console.error('Error initializing Sequelize:', error);
+            throw error;
+          }
+    }
--- src/utils/emailUtilityHelper.ts
+++ src/utils/emailUtilityHelper.ts
@@ -1,16 +1,20 @@
 import * as nodemailer from 'nodemailer';
+import  {getSecret} from '../utils/secretsManager';
 
 export async function sendEmailNotification(mailOptions:any) {
   // create reusable transporter object using the default SMTP transport
+  const dbSecrets = await getSecret('ReciterPubNotifierDev');
+  const { SMTP_HOST_NAME,SMTP_ADMIN_EMAIL,SMTP_PASSWORD,SMTP_USER,SMTP_PORT, SMTP_SECURE } = dbSecrets;
+
   const transporter = nodemailer.createTransport({
-    host:  process.env.SMTP_HOST_NAME,  
-      port: process.env.NODE_ENV === "production"  ? 465 : 25,
-      secure: process.env.NODE_ENV === "production" ? true : false,
+    host:  SMTP_HOST_NAME || process.env.SMTP_HOST_NAME,  
+      port: process.env.NODE_ENV === "production"  ? SMTP_PORT : 25,
+      secure: process.env.NODE_ENV === "production" ? SMTP_SECURE : false,
       logger: true,
       debug: true,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
+        user: SMTP_USER || process.env.SMTP_USER,
+        pass: SMTP_PASSWORD || process.env.SMTP_PASSWORD
       },
   });
 
@@ -21,8 +25,8 @@ export async function sendEmailNotification(mailOptions:any) {
     subject: mailOptions.subject,
     html: mailOptions.html
   }).then((info) => {
-    console.info(`Mail sent successfully!!`);
-    console.info(`[MailResponse]=${info.response} [MessageID]=${info.messageId}`);
+    console.log(`Mail sent successfully!!`);
+    console.log(`[MailResponse]=${info.response} [MessageID]=${info.messageId}`);
     return info;
 });
 }
