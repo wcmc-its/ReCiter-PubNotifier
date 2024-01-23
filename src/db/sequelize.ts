82b9ca113744ed736a376fb9e4893417730a441a
mrj4001 <mrj4001@med.cornell.edu>
2024-01-23 08:29:44 +0530

    refactored sequelize and models code

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,10 +1,8 @@
-import { Sequelize } from "sequelize";
-import {initializeSequelize} from "../config/db.config";
-import { initModels } from './models/init-models';
+import sequelize from "../config/db.config";
+import {initModels}  from './models/init-models';
 
 
-export async function initSequelizeModels() {
-const sequelize:Sequelize = await  initializeSequelize();  
- const models = initModels(sequelize);
- return models;
-}
+
+const models = initModels(sequelize);
+
+export default models;
