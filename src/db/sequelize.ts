89e94cc6df612b846fb843250dbb132b1cb9f64b
mrj4001 <mrj4001@med.cornell.edu>
2024-01-24 09:08:16 +0530

    Added AWSSecretManager code back

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,6 +1,10 @@
-import {sequelize} from "../config/db.config";
+import { Sequelize } from "sequelize";
+import {initializeSequelize} from "../config/db.config";
 import { initModels } from './models/init-models';
 
-const models = initModels(sequelize);
 
-export default models;
+export async function initSequelizeModels() {
+const sequelize:Sequelize = await  initializeSequelize();  
+ const models = initModels(sequelize);
+ return models;
+}
