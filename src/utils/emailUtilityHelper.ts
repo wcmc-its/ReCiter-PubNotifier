ba2de46e3d0b187df501aa6ad249e8648f3b73e1
mrj4001 <mrj4001@med.cornell.edu>
2024-01-24 09:25:43 +0530

    Added AWS Secrets Code to email

--- src/utils/emailUtilityHelper.ts
+++ src/utils/emailUtilityHelper.ts
@@ -1,16 +1,26 @@
 import * as nodemailer from 'nodemailer';
+import  {getSecret} from '../utils/secretsManager';
 
 export async function sendEmailNotification(mailOptions:any) {
   // create reusable transporter object using the default SMTP transport
+  let dbSecrets:any ='';
+  const reciterPubNotifierSecretLoc = process.env.RECITER_PUB_NOTIFIER_SECRET_LOC || '';
+  if(reciterPubNotifierSecretLoc && reciterPubNotifierSecretLoc =='AWS')
+  {
+        const reciterPubSecretManager = process.env.RECITER_PUB_SECRET_MANAGER || '';
+        dbSecrets = await getSecret( reciterPubSecretManager);
+  }
+  const { SMTP_HOST_NAME,SMTP_ADMIN_EMAIL,SMTP_PASSWORD,SMTP_USER,SMTP_PORT, SMTP_SECURE } = dbSecrets;
+
   const transporter = nodemailer.createTransport({
-    host:  process.env.SMTP_HOST_NAME,  
-      port: process.env.NODE_ENV === "production"  ? Number(process.env.SMTP_PORT?process.env.SMTP_PORT:465) : 25,
-      secure: process.env.NODE_ENV === "production" ? Boolean(process.env.SMTP_SECURED?process.env.SMTP_SECURED:true) : false,
+    host:  SMTP_HOST_NAME || process.env.SMTP_HOST_NAME,  
+      port: process.env.NODE_ENV === "production"  ? (SMTP_PORT?SMTP_PORT:465) : 25,
+      secure: process.env.NODE_ENV === "production" ? (SMTP_SECURE?SMTP_SECURE:true) : false,
       logger: true,
       debug: true,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
+        user: SMTP_USER || process.env.SMTP_USER,
+        pass: SMTP_PASSWORD || process.env.SMTP_PASSWORD
       },
   });
 
@@ -21,8 +31,8 @@ export async function sendEmailNotification(mailOptions:any) {
     subject: mailOptions.subject,
     html: mailOptions.html
   }).then((info) => {
-    console.info(`Mail sent successfully!!`);
-    console.info(`[MailResponse]=${info.response} [MessageID]=${info.messageId}`);
+    console.log(`Mail sent successfully!!`);
+    console.log(`[MailResponse]=${info.response} [MessageID]=${info.messageId}`);
     return info;
 });
 }
