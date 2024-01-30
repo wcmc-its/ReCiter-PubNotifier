d507e8ebd498abd8abb24d5667a1d24c8521c209
mrj4001 <mrj4001@med.cornell.edu>
2024-01-30 17:03:26 +0530

    made changes for the codePipeline

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { sendPubEmailNotifications } from "./notifications/notifications.sendEmail.controller";
 export async function handler(event: any, context: any): Promise<any> {
   const data = await sendPubEmailNotifications();
+  
+  try {
+        await codepipeline.putJobSuccessResult({
+            jobId: event["ReCiter-PubNotifier.job"].id
+        }).promise();
+    } catch (err) {
+        console.error('Error calling PutJobSuccessResult:', err);
+        throw err; // Propagate the error
+    }
+
+    // Return any response if needed
+    return {
+        statusCode: 200,
+        body: 'Lambda function executed successfully!'
+    };
   return {
     statusCode: 200,
     body: data
