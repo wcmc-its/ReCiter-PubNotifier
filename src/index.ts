eba2d6975e3db82d6d0b495aabf3dfde52497f32
mrj4001 <mrj4001@med.cornell.edu>
2024-03-18 13:46:01 +0530

    made changes to fix the codepipeline issue

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,21 @@ const AWS = require('aws-sdk');
 
 export async function handler(event: any, context: any): Promise<any> {
   const codepipeline = new AWS.CodePipeline();	
-  const data = await sendPubEmailNotifications();
+  //const data = await sendPubEmailNotifications();
     
     // Retrieve the Job ID from the Lambda action
     var jobId = event["CodePipeline.job"].id;
-    
+    console.log('event************', event);
     // Retrieve the value of UserParameters from the Lambda action configuration in CodePipeline, in this case a URL which will be
     // health checked by this function.
-    var url = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
-    
-    // Notify CodePipeline of a successful job
-    var putJobSuccess = function(message:string) {
+	 // Notify CodePipeline of a successful job
+    var putJobSuccess = function(message:any) {
         var params = {
             jobId: jobId
         };
+		console.log('params',params)
         codepipeline.putJobSuccessResult(params, function(err:any, data:any) {
+			console.log('err and data',err,data);
             if(err) {
                 context.fail(err);      
             } else {
@@ -31,7 +31,7 @@ export async function handler(event: any, context: any): Promise<any> {
     
     // Notify CodePipeline of a failed job
     var putJobFailure = function(message:any) {
-        var params = {
+	    var params = {
             jobId: jobId,
             failureDetails: {
                 message: JSON.stringify(message),
@@ -39,79 +39,23 @@ export async function handler(event: any, context: any): Promise<any> {
                 externalExecutionId: context.awsRequestId
             }
         };
+		console.log('params from fail',params)
         codepipeline.putJobFailureResult(params, function(err:any, data:any) {
+			console.log('err and data from fail',err,data);
             context.fail(message);      
         });
     };
-    
-    // Validate the URL passed in UserParameters
-    if(!url || url.indexOf('http://') === -1) {
-        putJobFailure('The UserParameters field must contain a valid URL address to test, including http:// or https://');  
-        return;
-    }
-    
-    // Helper function to make a HTTP GET request to the page.
-    // The helper will test the response and succeed or fail the job accordingly 
-    var getPage = function(url:string, callback:any) {
-        var pageObject = {
-            body: '',
-            statusCode: 0,
-            contains: function(search:any) {
-                return this.body.indexOf(search) > -1;    
-            }
-        };
-        http.get(url, function(response:any) {
-            pageObject.body = '';
-            pageObject.statusCode = response.statusCode;
-            
-            response.on('data', function (chunk:any) {
-                pageObject.body += chunk;
-            });
-            
-            response.on('end', function () {
-                callback(pageObject);
-            });
-            
-            response.resume(); 
-        }).on('error', function(error:string) {
-            // Fail the job if our request failed
-            putJobFailure(error);    
-        });           
-    };
-    
-    getPage(url, function(returnedPage:any) {
-        try {
-            // Check if the HTTP response has a 200 status
-            assert(returnedPage.statusCode === 200);
-            // Check if the page contains the text "Congratulations"
-            // You can change this to check for different text, or add other tests as required
-            assert(returnedPage.contains('Congratulations'));  
-            
-            // Succeed the job
-            putJobSuccess("Tests passed.");
-        } catch (ex) {
-            // If any of the assertions failed then fail the job
-            putJobFailure(ex);    
-        }
-    });
- /* try {
-        await codepipeline.putJobSuccessResult({
-            jobId: event["ReciterPubMgrEmailNotificationTrigger.job"].id
-        }).promise();
-    } catch (err) {
-        console.error('Error calling PutJobSuccessResult:', err);
-        throw err; // Propagate the error
-    }
 
-    // Return any response if needed
-    return {
-        statusCode: 200,
-        body: 'Lambda function executed successfully!'
-    };
-  return {
-    statusCode: 200,
-    body: data
-  };*/
+    try { 
+        const data = await sendPubEmailNotifications();
+		console.log("Completed Job execution and moving to Job Success",data);
+        // Succeed the job
+        putJobSuccess("Tests passed.");
+    } catch (ex) {
+        // If any of the assertions failed then fail the job
+		console.log("Completed Job execution and moving to Job failed");
+        putJobFailure(ex);    
+    }   
 }
 
 
